2036949aff9855be98bf0ce522f5f9a52d36292f
scqilin <1211wuqiong@sina.com>
2018-08-18 15:11:03 +0800

    update
    design mode

---
+++ javascript/designmode/webpack.dev.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:__dirname,
+        filename:'./release/bundle.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./index.html'
+        })
+    ],
+    devServer:{
+        contentBase:path.join(__dirname,'./release'),
+        open:true,
+        port:8000
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    }
+}
